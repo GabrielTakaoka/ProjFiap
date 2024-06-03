6417cebcf98bdc39de8dbf8a2f5c035015119801
Gabriel Takaoka <90276542+GabrielTakaoka@users.noreply.github.com>
2024-06-02 22:58:01 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
 function showOptions(optionsId, button) {
-    // Esconde todas as opções
+
     var options = document.querySelectorAll('.options');
     options.forEach(function(option) {
         option.style.display = 'none';
     });
     
-    // Mostra as opções correspondentes ao botão clicado
+
     var selectedOptions = document.getElementById(optionsId);
     selectedOptions.style.display = 'block';
     
-    // Remove a classe 'selected' de todos os botões
+
     var buttons = document.querySelectorAll('.container button');
     buttons.forEach(function(btn) {
         btn.classList.remove('selected');
     });
     
-    // Adiciona a classe 'selected' ao botão clicado
+
     button.classList.add('selected');
+}
+function openNotification() {
+    document.getElementById('notification-popup').style.display = 'block';
+}
+function closeNotification() {
+    document.getElementById('notification-popup').style.display = 'none';
 }
