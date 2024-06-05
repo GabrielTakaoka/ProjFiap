78a946d613a08b013a91f729b71bfad0022dc660
Gabriel Takaoka <90276542+GabrielTakaoka@users.noreply.github.com>
2024-06-05 00:03:57 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -24,3 +24,13 @@ function openNotification() {
 function closeNotification() {
     document.getElementById('notification-popup').style.display = 'none';
 }
+function changeProfilePicture(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profile-img').src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+}
